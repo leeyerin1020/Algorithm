cfc569ee3bd34c8fbe31bdb03d359dccc00388b9
leeyerin1020 <69492031+leeyerin1020@users.noreply.github.com>
2021-08-10 10:03:06 +0900

    5_1-5_2

--- Inflearn/cotest5_1.java
+++ Inflearn/cotest5_1.java
@@ -1,3 +1,4 @@
+//올바른 괄호
 package Unit5;
 import java.util.Scanner;
 public class cotest5_1{
@@ -18,7 +19,12 @@ public class cotest5_1{
 		
 		if(num!=num2) {answer="NO";}
 		System.out.println(answer);
-		
+		/*
+                           괄호, 대괄호 나오는 문제는 대부분 Stack을 이용한다!
+            Stack<Character> stack= new Stack<>();// 스택을 선언함
+            stack.push(x), stack.pop(x), stack.isEmpty함수를 이용하여 처리해준다!
+            -> '('인 경우 push해주고 else인 경우 isEmpty여부 확인하여 pop or return 이런식으로! 
+		 */
 	
 	}
 
--- Inflearn/cotest5_2.java
+++ Inflearn/cotest5_2.java
@@ -1,9 +1,23 @@
+//괄호문자제거-> 괄호관련 문제는 거의 stack을 이용한다
 package Unit5;
 import java.util.*;
 public class cotest5_2{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String str= scanner.next();
+		Stack<Character> stack= new Stack<>();
+		String answer="";
+		for(char x: str.toCharArray()) {
+			if(x==')') {
+				while(stack.pop()!='(');// while문 조건을 이렇게 주면 -> '('인 경우까지 pop하고 종료되지!
+			}// while문 하고서 '('이거 빼려고 또 pop할 필요 없다는 의미임!
+			else stack.push(x);
+		}
+		for(int i=0; i<stack.size();i++) {
+			System.out.print(stack.get(i));
+		}
+		
+		/*-> 스택 이용하지 않은 내방법
 		ArrayList<Character> answer= new ArrayList<>();
 		int num=0;
 		for(char x: str.toCharArray()) {
@@ -17,6 +31,7 @@ public class cotest5_2{
 		for(char x: answer) {
 		System.out.print(x);
 		}
+		*/
 	}
 
 }
