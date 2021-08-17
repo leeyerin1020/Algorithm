c4adff483350ff9222b18ce2d2e5554edc6b0a71
leeyerin1020 <69492031+leeyerin1020@users.noreply.github.com>
2021-08-17 21:52:14 +0900

    6_5

--- Inflearn/cotest6_5.java
+++ Inflearn/cotest6_5.java
@@ -1,5 +1,6 @@
 package Unit6;
-//중복 확인
+//중복 확인-> "해쉬맵" 이용하면, key값,value값 중복되면 안된다는 논리 적용해서 풀 수 있음!!
+// Arrays.sort해서 배열을 오름차순으로 정렬한 후 for문 이용해서 같은 숫자 있으면 중복이라는 논리도 !
 import java.util.*;
 public class cotest6_5{
 	public static void main(String[] args) {
