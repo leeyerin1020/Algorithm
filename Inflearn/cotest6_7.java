f2f0521acd466253a77935e7d9d57698f41574d0
leeyerin1020 <69492031+leeyerin1020@users.noreply.github.com>
2021-08-20 21:54:41 +0900

    6_7

--- Inflearn/cotest6_7.java
+++ Inflearn/cotest6_7.java
@@ -1,9 +1,32 @@
 package Unit6;
 import java.util.*;
+//강사님은 좌표가 나오는 경우, 좌표를 저장하는 클래스를 만들어서 처리해준다고 하심
+class Point implements Comparable<Point>{// Comparable 인터페이스 implements하여 정렬할 때 이용한다.
+	public int x, y;
+	Point(int x, int y){// 좌표 받는 Point 객체, 초기화해주는 생성자 
+		this.x=x;
+		this.y=y;
+	}
+	@Override
+	public int compareTo(Point o) {// compareTo 함수를 꼭 override해줘야함
+		if(this.x==o.x) return this.y-o.y;
+		else return this.x-o.x;
+	}
+}
 public class cotest6_7 {
 	public static void main(String[] args) {
 		Scanner scanner= new Scanner(System.in);
 		int n= scanner.nextInt();
+		//좌표라는 객체를 받는 arrayList를 선언해줭함!
+		ArrayList<Point> list= new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			int x= scanner.nextInt();
+			int y= scanner.nextInt();
+			list.add(new Point(x,y));
+		}
+		Collections.sort(list);
+		
+		/*내가 푼 방법 왜 틀렸을까
 		int[][] list= new int[n][2];
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<2; j++) {
@@ -40,6 +63,7 @@ public class cotest6_7 {
 			}
 			System.out.println();
 		}
+		*/
 	}
 
 }
