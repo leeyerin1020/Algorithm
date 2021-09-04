11cdeb204a7eb93226bce51e2a2d0a782f318308
leeyerin1020 <69492031+leeyerin1020@users.noreply.github.com>
2021-09-04 22:50:43 +0900

    7_4-7_5

--- Inflearn/Cotest7_4.java
+++ Inflearn/Cotest7_4.java
@@ -2,19 +2,27 @@ package unit7;
 //피보나치 재귀
 import java.util.*;
 public class Cotest7_4 {
-	public int func(int n) {
-		if(n<=2) {
-			System.out.print(1+" ");
-			return 1;
-		}
-		else {
+	int sum=0;
+	public void func(int n) {
+		if(n>=1)System.out.print("1"+" ");
+		int a=0;
+		int b=1;
+		while(n>1) {
+			System.out.print((a+b) +" ");
+			int c=b;
+			b=a+b;
+			a=c;
+			n--;
 			
 		}
+		
 	}
 	public static void main(String[] args) {
 		Scanner scanner= new Scanner(System.in);
 		int n= scanner.nextInt();
-		
+		Cotest7_4 c= new Cotest7_4();
+		c.func
+		(n);
 	}
 
 }
